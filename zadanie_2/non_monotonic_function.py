28b65852829dc033da42632d1539781fcb481bbb
zlatatikhomirova <72922674+zlatatikhomirova@users.noreply.github.com>
2022-02-16 14:26:53 +0700

    Update non_monotonic_function.py

--- zadanie_2/non_monotonic_function.py
+++ zadanie_2/non_monotonic_function.py
@@ -25,6 +25,30 @@ def imp(a, b):
     """
     return not a or b
 
+def to_py(expr: str) -> str:
+    """
+    Функция преобразует исходное выражение в выражение, которое воспринимается интерпретатором Python.
+    Поддерживает большинство логических операций, кроме импликации, стрелок Пирса, штриха Шеффера.
+    
+    Импликацию пропишем вручную.
+    """
+    log_operations = {'¬': " not ", '∧': " and ", '∨': " or ", '≡': " == ", '⊻': '^', '⊕': '^'} 
+    for el in log_operations:
+        try:
+            expr.replace(el, log_operations[el])
+        except KeyError:
+            continue
+    return expr
+
+def log_table(n: int, params: list, expr: str) -> PrettyTable:
+    """
+    n - количество переменных в логическом выражении.
+    params - переменные в логическом выражении.
+    expr - выражение, которое нужно вычислить.
+    """
+    possible_values = list(product((0,1), repeat=n))
+    
+    
 
 txt = """0	0	0	0
 0	0	1	1
@@ -34,7 +58,9 @@ txt = """0	0	0	0
 1	0	1	0
 1	1	0	0
 1	1	1	1"""
+
 check = []
+
 for elem in txt.split('\n'):
   check.append([int(i) for i in elem.split('\t')])
 
@@ -44,10 +70,9 @@ n = 3 # количество переменных
 
 
 statement = "(¬x ∧ y ∧ z) ∨ (¬x ∧ ¬y ∧ z) ∨ (¬x ∧ ¬y ∧ ¬z)"
-log_operations = {'¬': " not ", '∧': " and ", '∨': " or ", '→': "!", '≡': " == ", '⊻': '^', '⊕': '^'}  
-tbl = statement.maketrans(log_operations)
-expr_default = statement.translate(tbl)
-
+# log_operations = {'¬': " not ", '∧': " and ", '∨': " or ", '→': "!", '≡': " == ", '⊻': '^', '⊕': '^'}  
+# tbl = statement.maketrans(log_operations)
+# expr_default = statement.translate(tbl)
 del_log = {a[0]: '' for a in log_operations.items()}
 tbl_1 = statement.maketrans(del_log)
 chr_d = statement.translate(tbl_1)
